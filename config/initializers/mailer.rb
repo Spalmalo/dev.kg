3cd696f9905842f5c228b421a1a22fa6865acaec
Rustam Sharshenov <rustam@sharshenov.com>
2015-01-28 21:45:32 +0600

    Disable mailer

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -1,13 +1,13 @@
-Rails.application.config.action_mailer.default_url_options = {
-  host: ENV["SITE_DOMAIN"]
-}
+# Rails.application.config.action_mailer.default_url_options = {
+#   host: ENV["SITE_DOMAIN"]
+# }
 
-Rails.application.config.action_mailer.smtp_settings = {
-  address:              ENV["EMAIL_HOST"],
-  port:                 (ENV["EMAIL_PORT"] || 587),
-  domain:               ENV["SITE_DOMAIN"],
-  user_name:            ENV["EMAIL_LOGIN"],
-  password:             ENV["EMAIL_PASSWORD"],
-  authentication:       :plain,
-  enable_starttls_auto: true
-}
+# Rails.application.config.action_mailer.smtp_settings = {
+#   address:              ENV["EMAIL_HOST"],
+#   port:                 (ENV["EMAIL_PORT"] || 587),
+#   domain:               ENV["SITE_DOMAIN"],
+#   user_name:            ENV["EMAIL_LOGIN"],
+#   password:             ENV["EMAIL_PASSWORD"],
+#   authentication:       :plain,
+#   enable_starttls_auto: true
+# }
