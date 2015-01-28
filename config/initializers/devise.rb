6763f41ce50462a121116143e315c8c54cdfcad7
Rustam Sharshenov <rustam@sharshenov.com>
2015-01-28 14:00:05 +0600

    oops

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -10,7 +10,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "#{ I18n.t('layout.sitename') } <noreply@#{ ENV['SITE_DOMAIN'] }>"
+  config.mailer_sender = Proc.new { "#{ I18n.t('layout.sitename') } <noreply@#{ ENV['SITE_DOMAIN'] }>" }
 
   # Configure the class responsible to send e-mails.
   # config.mailer = 'Devise::Mailer'
