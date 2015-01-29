7a32bdf6fdd14e81eaf0e06c60963b37f4db425c
Rustam Sharshenov <rustam@sharshenov.com>
2015-01-29 23:25:09 +0600

    Add ability to set basic auth with env vars

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  http_basic_authenticate_with name: ENV["BASIC_AUTH_USER"], password: ENV["BASIC_AUTH_PASS"] if ENV["BASIC_AUTH_USER"] and ENV["BASIC_AUTH_PASS"]
+
   rescue_from ActiveRecord::RecordNotFound do |ex|
     render '404', status: 404
   end
