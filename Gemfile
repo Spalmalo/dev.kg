1278337132e0431e6231dd53786a5b6847f5de6d
Rustam Sharshenov <rustam@sharshenov.com>
2015-01-31 15:08:35 +0600

    Use codeclimate test reporter on travis

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ group :development, :test do
 end
 
 group :test do
+  gem "codeclimate-test-reporter", require: nil
   gem 'rspec-rails'
   gem 'factory_girl_rails'
   gem 'shoulda-matchers'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -4,6 +4,10 @@ require 'spec_helper'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'shoulda/matchers'
+require "codeclimate-test-reporter"
+
+CodeClimate::TestReporter.start
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
