e806439cf5c62fd0a77729f09dfb74b2caede54c
Rustam Sharshenov <rustam@sharshenov.com>
2015-02-01 15:37:04 +0600

    Validate format of reference url

--- app/models/reference.rb
+++ app/models/reference.rb
@@ -3,5 +3,6 @@ class Reference < ActiveRecord::Base
 
   validates_presence_of :video, :url
   validates_length_of :title, maximum: 255
+  validates_format_of :url, with: URI.regexp(["http", "https", "ftp"])
 
 end
--- spec/models/reference/validations_spec.rb
+++ spec/models/reference/validations_spec.rb
@@ -6,6 +6,11 @@ describe Reference, type: :model do
 
   it { is_expected.to validate_length_of(:title).is_at_most(255) }
 
+  it { is_expected.not_to allow_value("wrong").for(:url) }
+  it { is_expected.to allow_value("ftp://some.ftp/server").for(:url) }
+  it { is_expected.to allow_value("http://some.http/server").for(:url) }
+  it { is_expected.to allow_value("https://some.https/server").for(:url) }
+
   it "should have valid vactory" do
     reference = build(:reference)
     expect(reference).to be_valid
