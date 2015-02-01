23fac63bf79547ef98c81e9940c205c0bc3aa2fe
Rustam Sharshenov <rustam@sharshenov.com>
2015-02-01 13:45:03 +0600

    Update references test in order to remove deprecation warning

--- spec/models/reference/validations_spec.rb
+++ spec/models/reference/validations_spec.rb
@@ -4,7 +4,7 @@ describe Reference, type: :model do
   it { is_expected.to validate_presence_of :video }
   it { is_expected.to validate_presence_of :url }
 
-  it { is_expected.to ensure_length_of(:title).is_at_most(255) }
+  it { is_expected.to validate_length_of(:title).is_at_most(255) }
 
   it "should have valid vactory" do
     reference = build(:reference)
