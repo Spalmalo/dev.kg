69a96a83e63ccf744063b838b4ec944b5d9de32f
Rustam Sharshenov <rustam@sharshenov.com>
2015-03-07 18:00:33 +0600

    Make tests output cleaner

--- spec/support/warden.rb
+++ spec/support/warden.rb
@@ -1,5 +1,6 @@
 module WardenControllerHelpers
   def login_as user
+    allow_message_expectations_on_nil
     allow(request.env['warden']).to receive(:authenticate!).and_return(user)
     allow(controller).to receive(:current_user).and_return(user)
   end
