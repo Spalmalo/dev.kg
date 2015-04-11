d58fd4aeaab42cd0d86adb5274f980d0872dbd8d
Rustam Sharshenov <rustam@sharshenov.com>
2015-04-11 09:33:59 +0600

    Update gems

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,9 +2,9 @@ FactoryGirl.define do
   factory :user do
     provider "github"
     sequence(:uid) { |i| i }
-    sequence(:nickname) { Faker::Internet.user_name }
-    sequence(:email)    { Faker::Internet.email }
-    sequence(:name)     { Faker::Name.name }
+    sequence(:nickname) { FFaker::Internet.user_name }
+    sequence(:email)    { FFaker::Internet.email }
+    sequence(:name)     { FFaker::Name.name }
     image "http://some/image/url"
   end
 end
