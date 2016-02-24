3336eb8b5c3499bb13b04def292232227469ab37
Rustam Sharshenov <rustam@sharshenov.com>
2016-02-24 21:42:20 +0600

    Update gems

--- spec/models/video/validations_spec.rb
+++ spec/models/video/validations_spec.rb
@@ -12,9 +12,14 @@ describe Video, type: :model do
 
   it { is_expected.to validate_length_of(:title).is_at_most(255) }
   it { is_expected.to validate_length_of(:description).is_at_most(4000) }
-
-  it { is_expected.to validate_uniqueness_of :video_url }
   it { is_expected.to have_readonly_attribute :video_url }
+
+
+  it "should validate uniqueness of video_url" do
+    create :video, video_url: "https://www.youtube.com/watch?v=qpgT_62y5kQ"
+
+    expect(build :video, video_url: "https://www.youtube.com/watch?v=qpgT_62y5kQ").not_to be_valid
+  end
 
   it "should have valid vactory" do
     screencast = build(:screencast)
